29bde356265ba937e4903e69d93d1a15d74e7f9a
wilrak0v <wilrakov@proton.me>
2026-03-13 20:04:57 +0100

    feat(micro_lib.c): lecture des fonctions

--- src/micro_lib.c
+++ src/micro_lib.c
@@ -10,16 +10,17 @@ void get_lib(FILE *f, Mc *mc)
         return;
     }
 
-    uint32_t current_fn;
+    uint32_t current_fn_addr;
     int content_size = 0;
-    while (fread(&current_fn, sizeof(current_fn), 1, f) == 1)
+    while (fread(&current_fn_addr, sizeof(current_fn_addr), 1, f) == 1)
     {
-        if (current_fn == 0xFFFFFFFF)
+        if (current_fn_addr == 0xFFFFFFFF)
         {
             printf("That's the end at pos %d\n", mc->pc);
             break;
         }
         content_size++;
         // READ the addr and add it into linked table
+        printf("Fn '%d' at addr '%d'\n", content_size, current_fn_addr);
     }
 }
